d8cc24e2742ef9989bb35811cab9cfbe4810ffdb
na3t1e <R@zR@b0tka>
2022-06-01 22:50:55 +0300

    make refactoring

--- lib/main.dart
+++ lib/main.dart
@@ -69,75 +69,28 @@ class _MapPageState extends State<MapPage> {
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             /// поле ввода Х
-            Container(
-              decoration: BoxDecoration(
-                  borderRadius: const BorderRadius.all(
-                    Radius.circular(10),
-                  ),
-                  border: Border.all(
-                    width: 1,
-                    color: Colors.blueGrey,
-                  )),
-              padding: const EdgeInsets.only(left: 8),
-              width: 100,
-              child: TextField(
-                keyboardType:
-                    const TextInputType.numberWithOptions(decimal: true),
-                inputFormatters: [
-                  FilteringTextInputFormatter.allow(RegExp('[0-9.,]')),
-                ],
-                onChanged: (value) => lat = double.parse(value),
-                controller: latController,
-                cursorColor: color,
-                decoration: const InputDecoration(
-                  border: InputBorder.none,
-                  hintText: "X",
-                ),
-              ),
-            ),
+            Input(controller: latController, color: color, text: "X"),
+
             /// поле ввода Y
-            Container(
-              decoration: BoxDecoration(
-                  borderRadius: const BorderRadius.all(
-                    Radius.circular(10),
-                  ),
-                  border: Border.all(
-                    width: 1,
-                    color: Colors.blueGrey,
-                  )),
-              padding: const EdgeInsets.only(left: 8),
-              width: 100,
-              child: TextField(
-                keyboardType:
-                    const TextInputType.numberWithOptions(decimal: true),
-                inputFormatters: [
-                  FilteringTextInputFormatter.allow(RegExp('[0-9.,]')),
-                ],
-                onChanged: (value) => long = double.parse(value),
-                controller: longController,
-                cursorColor: color,
-                decoration: const InputDecoration(
-                  border: InputBorder.none,
-                  hintText: "Y",
-                ),
-              ),
-            ),
+            Input(controller: longController, color: color, text: "Y"),
           ],
         ),
         actions: [
           /// кнопка для поиска плитки
           IconButton(
             onPressed: () {
+              lat = double.parse(latController.text);
+              long = double.parse(longController.text);
+
               ///рассчитываются поля плитки по формуле
-              if (lat != 0 && long != 0) {
-                setState(() {
+              setState(
+                () {
                   print((((pow(2, 19 + 8) / 2) * (1 + long! / 180)) / 256));
                   x = (((pow(2, 19 + 8) / 2) * (1 + long! / 180)) / 256)
                       .floor();
                   y = (((pow(2, 19 + 8) / 2) *
                               (1 -
-                                  log(
-                                      tan(pi / 4 + ((pi * lat!) / 180) / 2) *
+                                  log(tan(pi / 4 + ((pi * lat!) / 180) / 2) *
                                           pow(
                                               (1 -
                                                       0.0818191908426 *
@@ -154,8 +107,8 @@ class _MapPageState extends State<MapPage> {
 
                   url =
                       "https://core-carparks-renderer-lots.maps.yandex.net/maps-rdr-carparks/tiles?l=carparks&x=$x&y=$y&z=19&scale=1&lang=ru_RU";
-                });
-              }
+                },
+              );
             },
             icon: const Icon(Icons.search),
             iconSize: 32,
@@ -170,7 +123,7 @@ class _MapPageState extends State<MapPage> {
             children: [
               /// рассчитнанные координаты
               Text(
-                "Координаты плитки: $x   $y",
+                "Координаты плитки: $x   $y", style: TextStyle( fontSize: 24, fontWeight: FontWeight.w600,),
               ),
               /// картинка плитки
               Padding(
@@ -178,23 +131,83 @@ class _MapPageState extends State<MapPage> {
                 child: url != null
                     ? CachedNetworkImage(
                         imageUrl: url!,
-                        errorWidget: (context, url, error) => Container(
-                          height: 200,
-                          width: 200,
+                        errorWidget: (context, url, error) => Placeholder(
                           color: color,
-                          child: const Center(
-                              child: Text("Ошибка загрузки картинки")),
+                          text: "Ошибка загрузки картинки плитки",
                         ),
                       )
-                    : Container(
-                        height: 200,
-                        width: 200,
+                    : Placeholder(
                         color: colorLight,
-                        child: const Center(child: Text("Здесь будет  плитка")),
+                        text: "Здесь будет картинка плитки",
                       ),
               ),
             ],
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class Placeholder extends StatelessWidget {
+  Color color;
+  String text;
+
+  Placeholder({required this.color, required this.text, Key? key})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 200,
+      width: 200,
+      color: color,
+      child: Center(
+          child: Text(
+        text,
+        textAlign: TextAlign.center,
+        style: const TextStyle(
+            fontSize: 20, fontWeight: FontWeight.w300, color: Colors.blueGrey),
+      )),
+    );
+  }
+}
+
+class Input extends StatelessWidget {
+  TextEditingController controller;
+  Color color;
+  String text;
+
+  Input(
+      {required this.controller,
+      required this.color,
+      required this.text,
+      Key? key})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          borderRadius: const BorderRadius.all(
+            Radius.circular(10),
+          ),
+          border: Border.all(
+            width: 1,
+            color: Colors.blueGrey,
+          )),
+      padding: const EdgeInsets.only(left: 8),
+      width: 100,
+      child: TextField(
+        keyboardType: const TextInputType.numberWithOptions(decimal: true),
+        inputFormatters: [
+          FilteringTextInputFormatter.allow(RegExp('[0-9.,]')),
+        ],
+        controller: controller,
+        cursorColor: color,
+        decoration: InputDecoration(
+          border: InputBorder.none,
+          hintText: text,
         ),
       ),
     );
